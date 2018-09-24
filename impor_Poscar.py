7118f4602854f18041dcb410930045755e567fe3
Jherfson Castro <jherfson.castro@gmail.com>
2018-09-24 13:48:27 -0300

    Update impor_Poscar.py

--- impor_Poscar.py
+++ impor_Poscar.py
@@ -7,11 +7,9 @@ estrutura = Structure.from_file("POSCAR")
 
 
 remove = PartialRemoveSitesTransformation([range(0,63)], [1/63], algo=1)
-print(type(remove))
 
 
 result = remove.apply_transformation(estrutura, return_ranked_list=63)
-print(type(remove))
 
 
 for i, item in enumerate(result):
